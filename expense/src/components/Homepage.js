fc235b54e1efb3f98f6e8fd4e8b7390a09884648
Sahil-Veshin <veshinsahil04@gmail.com>
2025-01-31 19:01:53 +0530

    Homepage

--- expense/src/components/Homepage.js
+++ expense/src/components/Homepage.js
@@ -4,11 +4,41 @@ function Homepage() {
   return (
     <div className='container'>
       <div className=' leftcontainer'>
-        <p></p>
-        <img src={img1} alt=''/>
+        <p> Welcome to Expense Tracker</p>
+        <p> Manage  all your expense here</p>
+        <img src={img1} alt='' />
       </div>
       <div className=' rightcontainer'>
-        <p></p>
+        <div className='formcard'>
+          <h2 style={{alignSelf:'center'}}>Register</h2>
+          <div>
+            <label>
+              Name
+            </label>
+            <input type='text' placeholder='Enter your name' required />
+          </div>
+          <div>
+            <label>
+              Email
+            </label>
+            <input type="email" placeholder='Enter your email' required />
+          </div>
+          <div>
+            <label>
+              Password
+            </label>
+            <input type="password" placeholder='Enter your password' required />
+          </div>
+          <div>
+            <label>
+              Confirm password
+            </label>
+            <input type="password" placeholder='confirm  your password' required />
+          </div>
+          <div>
+            <button>submit</button>
+          </div>
+        </div>
       </div>
     </div>
   )
